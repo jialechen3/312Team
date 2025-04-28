a0340e28f57475af1355eec6018b8b92c114ea85
jiale <jiale1810418002@gmail.com>
2025-04-27 20:26:37 -0400

    Update: Removed local database for deploy purpose Change to port 8116

--- util/database.py
+++ util/database.py
@@ -5,10 +5,8 @@ from pymongo import MongoClient
 docker_db = os.environ.get('DOCKER_DB', "false").lower() == "true"
 
 # Set up MongoDB connection string
-if docker_db:
-    mongo_client = MongoClient("mongodb://mongo:27017")  # docker-compose service name
-else:
-    mongo_client = MongoClient("mongodb://localhost:27017")
+mongo_client = MongoClient("mongodb://mongo:27017")  # docker-compose service name
+
 
 # Access the database and collections
 db = mongo_client["mmo_game"]
